0f3e076d2d5f304c22328ec2acfd2eddd7fe4356
Infernio <infernio@icloud.com>
2019-10-21 18:48:34 +0200

    Support games that do not use 'My Documents'
    Prerequisite for Morrowind.

--- Mopy/bash/initialization.py
+++ Mopy/bash/initialization.py
@@ -133,8 +133,11 @@ def init_dirs(bashIni_, personal, localAppData, game_info):
                                                game_info.masterlist_dir)
 
     #  Personal
-    personal = getPersonalPath(bashIni_, personal)
-    dirs['saveBase'] = personal.join(u'My Games', game_info.fsName)
+    if game_info.uses_personal_folders:
+        personal = getPersonalPath(bashIni_, personal)
+        dirs['saveBase'] = personal.join(u'My Games', game_info.fsName)
+    else:
+        dirs['saveBase'] = dirs['app']
 
     #  Local Application Data
     localAppData = getLocalAppDataPath(bashIni_, localAppData)
