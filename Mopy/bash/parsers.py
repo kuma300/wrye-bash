7dc44b496a229a0d472c78e1881803f8ef2567f8
lojack5 <1458329+lojack5@users.noreply.github.com>
2019-11-03 11:57:27 -0700

    [wx] Deprecation warning:
    `wx.StockCursor` -> `wx.Cursor`

--- Mopy/bash/parsers.py
+++ Mopy/bash/parsers.py
@@ -4000,10 +4000,10 @@ class ModFile(object):
                         self.tops[label].load(ins, do_unpack and (topClass != MobBase))
                     else:
                         self.topsSkipped.add(label)
-                        insSeek(size-header.__class__.rec_header_size,1,type + '.' + label)
+                        insSeek(size-header.__class__.rec_header_size,1,type.encode('ascii') + b'.' + label)
                 except:
-                    print u'Error in',self.fileInfo.name.s
-                    deprint(u' ',traceback=True)
+                    deprint(u'Error in %s' % self.fileInfo.name.s,
+                            traceback=True)
                     break
                 subProgress(insTell())
         #--Done Reading
